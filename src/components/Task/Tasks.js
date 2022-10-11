02a46beed3d3c447ba6649d1e2d6a61a24c9e7bf
Jorge Chavez <jchavezpalma@bloomberg.net>
2022-10-11 04:11:01 -0700

    Add task chart

--- src/components/Task/Tasks.js
+++ src/components/Task/Tasks.js
@@ -2,6 +2,10 @@ import { Container } from "@mui/material";
 import { Task } from "./Task";
 import styled from 'styled-components';
 import { TitleModal } from '../TaskDialog/TaskDialog';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const TasksWrapper = styled.ul`
   list-style: none;
@@ -19,13 +23,46 @@ const Empty = styled.div`
   font-weight: bold;
 `;
 
+const ChartWrapper = styled.div`
+  width: 5em;
+`;
+
+const TopWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 2em;
+  h2 {
+    margin-right: auto;
+  }
+`;
+
 const Tasks = props => {
   const { tasks } = props;
+
+  const complete = tasks.filter(task => task.done === true).length;
+  const incomplete = tasks.length - complete;
+
+  const data = {
+    datasets: [
+      {
+        data: [complete, incomplete],
+        backgroundColor: ['#A362EA', '#F4A921'],
+        borderColor: ['#A362EA', '#F4A921'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <Container>
-      <TitleModal>
-        Tasks
-      </TitleModal>
+      <TopWrapper>
+        <TitleModal>
+          Tasks
+        </TitleModal>
+        <ChartWrapper>
+          {tasks && tasks.length > 0 && <Doughnut data={data} />}
+        </ChartWrapper>
+      </TopWrapper>
       <TasksWrapper className="list">
         { tasks && tasks.map((task, idx) => (
           <Task key={idx} {...props} task={task} />
